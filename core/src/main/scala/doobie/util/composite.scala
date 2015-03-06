c5566d0239f9c811a2582820e5532687b8f3c661
wedens <wedens@eldis.ru>
2015-03-06 11:07:00 +0600

    change get to StateT[RS.ResultSetIO, Int, A]

--- core/src/main/scala/doobie/util/composite.scala
+++ core/src/main/scala/doobie/util/composite.scala
@@ -27,14 +27,14 @@ object composite {
   trait Composite[A] { c =>
     val set: (Int, A) => PS.PreparedStatementIO[Unit]
     val update: (Int, A) => RS.ResultSetIO[Unit]
-    val get: Int => RS.ResultSetIO[A]
+    val get: StateT[RS.ResultSetIO, Int, A]
     val length: Int
     val meta: List[(Meta[_], NullabilityKnown)]
     final def xmap[B](f: A => B, g: B => A): Composite[B] =
       new Composite[B] {
         val set    = (n: Int, b: B) => c.set(n, g(b))
         val update = (n: Int, b: B) => c.update(n, g(b))
-        val get    = (n: Int) => c.get(n).map(f)
+        val get = c.get map f
         val length = c.length
         val meta   = c.meta
       }
@@ -54,7 +54,7 @@ object composite {
       new Composite[A] {
         val set = A.set
         val update = A.update
-        val get = A.get
+        val get = StateT[RS.ResultSetIO, Int, A](s => A.get(s).map((s + 1, _)))
         val length = 1
         val meta = List(A.meta)
       }
@@ -90,7 +90,7 @@ object composite {
       new Composite[H :: T] {
         val set = (i: Int, l: H :: T) => H.set(i, l.head) >> T.set(i + H.length, l.tail)
         val update = (i: Int, l: H :: T) => H.update(i, l.head) >> T.update(i + H.length, l.tail)
-        val get = (i: Int) => (H.get(i) |@| T.get(i + H.length))(_ :: _)
+        override val get = H.get >>= (h => T.get map (t => (h :: t)))
         val length = H.length + T.length
         val meta = H.meta ++ T.meta
       }
@@ -99,7 +99,7 @@ object composite {
       new Composite[HNil] {
         val set = (_: Int, _: HNil) => ().point[PS.PreparedStatementIO]
         val update = (_: Int, _: HNil) => ().point[RS.ResultSetIO]
-        val get = (_: Int) => (HNil : HNil).point[RS.ResultSetIO]
+        override val get = StateT[RS.ResultSetIO, Int, HNil](s => (s, HNil : HNil).point[RS.ResultSetIO])
         val length = 0
         val meta = Nil
       }

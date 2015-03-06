48f78fa62e211c44e503734940050c0f8994d3bf
wedens <wedens@eldis.ru>
2015-03-06 11:24:19 +0600

    change set to StateT[PS.PreparedStatementIO, Int, Unit]

--- core/src/main/scala/doobie/util/composite.scala
+++ core/src/main/scala/doobie/util/composite.scala
@@ -25,14 +25,14 @@ object composite {
 
   @implicitNotFound("Could not find or construct Composite[${A}].")
   trait Composite[A] { c =>
-    val set: (Int, A) => PS.PreparedStatementIO[Unit]
+    val set: A => StateT[PS.PreparedStatementIO, Int, Unit]
     val update: (Int, A) => RS.ResultSetIO[Unit]
     val get: StateT[RS.ResultSetIO, Int, A]
     val length: Int
     val meta: List[(Meta[_], NullabilityKnown)]
     final def xmap[B](f: A => B, g: B => A): Composite[B] =
       new Composite[B] {
-        val set    = (n: Int, b: B) => c.set(n, g(b))
+        val set = (b: B) => c.set(g(b))
         val update = (n: Int, b: B) => c.update(n, g(b))
         val get = c.get map f
         val length = c.length
@@ -52,7 +52,7 @@ object composite {
 
     implicit def fromAtom[A](implicit A: Atom[A]): Composite[A] =
       new Composite[A] {
-        val set = A.set
+        val set = (a: A) => StateT[PS.PreparedStatementIO, Int, Unit](s => A.set(s, a) map ((s + 1, _)))
         val update = A.update
         val get = StateT[RS.ResultSetIO, Int, A](s => A.get(s).map((s + 1, _)))
         val length = 1
@@ -88,18 +88,18 @@ object composite {
   trait HListComposite {
     implicit def hlistComposite[H, T <: HList](implicit H: Composite[H], T: Composite[T]): Composite[H :: T] =
       new Composite[H :: T] {
-        val set = (i: Int, l: H :: T) => H.set(i, l.head) >> T.set(i + H.length, l.tail)
+        val set = (l: H :: T) => H.set(l.head) >> T.set(l.tail)
         val update = (i: Int, l: H :: T) => H.update(i, l.head) >> T.update(i + H.length, l.tail)
-        override val get = H.get >>= (h => T.get map (t => (h :: t)))
+        val get = H.get >>= (h => T.get map (t => (h :: t)))
         val length = H.length + T.length
         val meta = H.meta ++ T.meta
       }
 
     implicit val hnilComposite: Composite[HNil] =
       new Composite[HNil] {
-        val set = (_: Int, _: HNil) => ().point[PS.PreparedStatementIO]
+        val set = (_: HNil) => StateT[PS.PreparedStatementIO, Int, Unit](s => (s, ()).point[PS.PreparedStatementIO])
         val update = (_: Int, _: HNil) => ().point[RS.ResultSetIO]
-        override val get = StateT[RS.ResultSetIO, Int, HNil](s => (s, HNil : HNil).point[RS.ResultSetIO])
+        val get = StateT[RS.ResultSetIO, Int, HNil](s => (s, HNil : HNil).point[RS.ResultSetIO])
         val length = 0
         val meta = Nil
       }
